439b927a640716164775d877712293e10843dc39
EmilDieterle <dieterleemil@gmail.com>
2022-10-12 21:49:33 +0200

    Verlinkung L01, L01 angefangen

---
+++ Steckbrief/L01_Zufallsgedicht/script.js
@@ -0,0 +1,19 @@
+"use strict";
+var L01_Zufallsgedicht;
+(function (L01_Zufallsgedicht) {
+    window.addEventListener("load", function () {
+        let subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+        let predicade = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+        let object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+        console.log(subject);
+        console.log(predicade);
+        console.log(object);
+        for (let index = 0; index <= subject.length; index++) {
+            //if (index == 0) {
+            //  index = 1;
+            // }
+            console.log(subject[index]);
+        }
+    });
+})(L01_Zufallsgedicht || (L01_Zufallsgedicht = {}));
+//# sourceMappingURL=script.js.map
---
+++ Steckbrief/L01_Zufallsgedicht/script.ts
@@ -0,0 +1,25 @@
+namespace L01_Zufallsgedicht {
+
+    window.addEventListener("load", function() {
+
+let subject: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let predicade: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let object: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+console.log(subject);
+console.log(predicade);
+console.log(object);
+
+
+for (let index: number = 0; index <= subject.length; index++) {
+    //if (index == 0) {
+      //  index = 1;
+   // }
+    console.log(subject[index]);
+    
+}
+
+
+});
+
+}
