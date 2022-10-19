a6d9cd292249064ebc014dff3ce5eacd8b448a73
EmilDieterle <dieterleemil@gmail.com>
2022-10-20 00:40:32 +0200

    L02 hochgeladen

---
+++ Steckbrief/L02_EventInspector/script.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+Aufgabe: <L02_EventListener>
+Name: <Emil Dieterle>
+Matrikel: <271526>
+Datum: <.10.2022>
+Quellen: <>
+*/
+var L02_EventInspector;
+(function (L02_EventInspector) {
+    window.addEventListener("load", function () {
+    });
+})(L02_EventInspector || (L02_EventInspector = {}));
+//# sourceMappingURL=script.js.map
---
+++ Steckbrief/L02_EventInspector/script.ts
@@ -0,0 +1,19 @@
+/*
+Aufgabe: <L02_EventListener>
+Name: <Emil Dieterle>
+Matrikel: <271526>
+Datum: <.10.2022>
+Quellen: <>
+*/
+
+namespace L02_EventInspector {
+    
+window.addEventListener("load", function () {
+
+
+
+
+
+});
+
+}
