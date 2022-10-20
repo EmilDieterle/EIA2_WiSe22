c2fa62d4f1da8a721984cf8c7e4066a9122fcad6
EmilDieterle <dieterleemil@gmail.com>
2022-10-20 12:48:42 +0200

    L02

--- Steckbrief/L02_EventInspector/script.js
+++ Steckbrief/L02_EventInspector/script.js
@@ -10,9 +10,28 @@ var L02_EventInspector;
 (function (L02_EventInspector) {
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
+        document.addEventListener("keyup", logInfo);
+        document.addEventListener("click", logInfo);
         let div0 = document.querySelector("#div0");
-        div0.addEventlistener("click", placeSpan);
-        document.addEventListener("keydown", chooseSpan);
+        div0.addEventListener("click", logInfo);
+        div0.addEventListener("keyup", logInfo);
+        let div1 = document.querySelector("#div1");
+        div1.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        let body = document.querySelector("body");
+        body.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
     }
 })(L02_EventInspector || (L02_EventInspector = {}));
+function setInfoBox(_event) {
+    let target = document.querySelector("span");
+    let x = _event.clientX + 10 + "px";
+    let y = _event.clientY + 10 + "px";
+    target.innerHTML = x + " " + y + " " + _event.target;
+    target.style.top = y;
+    target.style.left = x;
+}
+function logInfo(_event) {
+    console.log(target);
+}
 //# sourceMappingURL=script.js.map
--- Steckbrief/L02_EventInspector/script.ts
+++ Steckbrief/L02_EventInspector/script.ts
@@ -7,17 +7,42 @@ Quellen: <>
 */
 
 namespace L02_EventInspector {
-    
-window.addEventListener("load", handleLoad);
 
-function handleLoad(_event: Event): void {
-        
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+        document.addEventListener("keyup", logInfo);
+        document.addEventListener("click", logInfo);
+
         let div0: HTMLElement = <HTMLElement>document.querySelector("#div0");
-        div0.addEventlistener("click", placeSpan);
-        document.addEventListener("keydown", chooseSpan);
+        div0.addEventListener("click", logInfo);
+        div0.addEventListener("keyup", logInfo);
 
 
+        let div1: HTMLElement = <HTMLElement>document.querySelector("#div1");
+        div1.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+
+        let body: HTMLElement = <HTMLElement>document.querySelector("body");
+        body.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
     }
+}
 
+function setInfoBox(_event: MouseEvent): void {
 
+    let target: HTMLSpanElement = <HTMLSpanElement>document.querySelector("span");
+
+    let x: string = _event.clientX + 10 + "px";
+    let y: string = _event.clientY + 10 + "px";
+
+    target.innerHTML = x + " " + y + " " + _event.target;
+
+    target.style.top = y;
+    target.style.left = x;
+}
+
+function logInfo(_event: Event): void {
+
+    console.log(target);
 }
