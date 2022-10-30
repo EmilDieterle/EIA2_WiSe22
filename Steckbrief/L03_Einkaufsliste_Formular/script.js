e7f6d7770f111141a7ad758aa4939e60d150baa0
EmilDieterle <dieterleemil@gmail.com>
2022-10-30 08:18:31 +0100

    L03

--- Steckbrief/L03_Einkaufsliste_Formular/script.js
+++ Steckbrief/L03_Einkaufsliste_Formular/script.js
@@ -6,11 +6,17 @@ Matrikel: <271526>
 Datum: <29.10.2022>
 */
 window.addEventListener("load", function () {
-    document.querySelector(".fa-regular fa-circle").addEventListener("click", function () {
+    document.querySelector(".fa-regular fa-circle")?.addEventListener("click", function () {
         console.log("fa-regular fa-circle ändert sich in fa-regular fa-circle-xmark");
     });
-    document.getElementById("#btn").addEventListener("click", function () {
+    document.getElementById("#btn")?.addEventListener("click", function () {
         console.log("Ein ListItem wird hinzugefügt");
+    });
+    document.querySelector("fa-solid fa-trash-can")?.addEventListener("click", function () {
+        console.log("Die Liste in der sich die Trash- can befindet, wird entfernt");
+    });
+    document.getElementById("#input")?.addEventListener("click", function () {
+        console.log("Hier kann ein ListItem hinzugefügt werden");
     });
 });
 //# sourceMappingURL=script.js.map
--- Steckbrief/L03_Einkaufsliste_Formular/script.ts
+++ Steckbrief/L03_Einkaufsliste_Formular/script.ts
@@ -7,16 +7,27 @@ Datum: <29.10.2022>
 
 window.addEventListener("load", function (): void {
 
-document.querySelector(".fa-regular fa-circle").addEventListener("click", function(){
+document.querySelector(".fa-regular fa-circle")?.addEventListener("click", function(){
 
 console.log("fa-regular fa-circle ändert sich in fa-regular fa-circle-xmark");
 
 });
 
-document.getElementById("#btn").addEventListener("click", function(){
+document.getElementById("#btn")?.addEventListener("click", function(){
 
     console.log("Ein ListItem wird hinzugefügt");
 });
+
+document.querySelector("fa-solid fa-trash-can")?.addEventListener("click", function(){
+    console.log("Die Liste in der sich die Trash- can befindet, wird entfernt")
+});
+
+document.getElementById("#input")?.addEventListener("click", function(){
+
+    console.log("Hier kann ein ListItem hinzugefügt werden");
+});
+
+
 
 
 
