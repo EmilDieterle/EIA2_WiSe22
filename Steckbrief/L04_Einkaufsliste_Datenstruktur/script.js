418e54ad7d5097995000d2ef88d23c3bd5f2f404
EmilDieterle <dieterleemil@gmail.com>
2022-11-03 12:59:57 +0100

    L04

--- Steckbrief/L04_Einkaufsliste_Datenstruktur/script.js
+++ Steckbrief/L04_Einkaufsliste_Datenstruktur/script.js
@@ -5,95 +5,84 @@ Name: <Emil Dieterle>
 Matrikel: <271526>
 Datum: <.10.2022>
 */
-window.addEventListener("load", function () {
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
     let itemContainer = document.getElementById("itemContainer");
-    let selectorButton = document.getElementById("#btn");
     let selectorInput = document.getElementById("#inputID");
-    selectorInput.addEventListener("click", function () {
-        console.log("Hier kann ein Item hinzugefügt werden.");
-    });
-    let selectorAmount = document.getElementById("#amountID");
-    selectorAmount.addEventListener("click", function () {
-        console.log("Hier kann eine Zahlenmenge des Item hinzugefügt werden");
-    });
-    let selectorComment = document.getElementById("#textareaID");
-    selectorComment.addEventListener("click", function () {
-        console.log("Hier kann ein Kommentar zum Item erstellt werden");
-    });
-    let selectorDate = document.getElementById("#dateID");
-    selectorDate.addEventListener("click", function () {
-        console.log("Hier kann ein Datum zum Item hinzugefügt werden");
-    });
-    selectorButton.addEventListener("click", function () {
-        console.log("Ein neues Div wird erschaffen. Die vorher eingegebenen Werte werden als <p> zu Kindern und <i> Elemente werden hinzugefügt");
-        let uncheckedCircle = document.createElement("i");
-        uncheckedCircle.classList.add("fa-regular fa-circle");
-        itemContainer.appendChild(uncheckedCircle);
-        let checkedCircle = document.createElement("i");
-        checkedCircle.classList.add(".hidden");
-        itemContainer.appendChild(checkedCircle);
-        let name = document.createElement("p");
-        name.classList.add("addedItem");
-        name.innerText = selectorInput.value;
-        itemContainer.appendChild(name);
-        selectorInput.value = "";
-        let amount = document.createElement("p");
-        amount.classList.add("addedAmount");
-        amount.innerText = selectorAmount.value;
-        itemContainer.appendChild(amount);
-        selectorAmount.value = "";
-        let comment = document.createElement("p");
-        comment.classList.add("addedComment");
-        comment.innerText = selectorComment.value;
-        itemContainer.appendChild(comment);
-        selectorComment.value = "";
-        let date = document.createElement("p");
-        date.classList.add("addedDate");
-        date.innerText = selectorDate.value;
-        itemContainer.appendChild(date);
-        selectorDate.value = "";
-        let edit = document.createElement("i");
-        edit.classList.add("fa-solid fa-pen-to-square");
-        itemContainer.appendChild(edit);
-        let trash = document.createElement("i");
-        edit.classList.add("fa-solid fa-trash-can");
-        itemContainer.appendChild(trash);
-        let item = document.createElement("div");
-        item.classList.add("addedDiv");
-        item.appendChild(uncheckedCircle);
-        item.appendChild(checkedCircle);
-        item.appendChild(name);
-        item.appendChild(amount);
-        item.appendChild(comment);
-        item.appendChild(date);
-        item.appendChild(edit);
-        item.appendChild(trash);
-        itemContainer.appendChild(item);
-    });
-    //let selectorRegularCircle: HTMLElement = document.querySelector(".fa-regular fa-circle")as unknown as HTMLElement;
-    uncheckedCircle.addEventListener("click", function () {
-        console.log("fa-regular fa-circle erhält die Klasse hidden von fa-regular fa-circle-xmark und fa-regular fa-circle-xmark ist nun sichtbar");
+    let selectorButton = document.getElementById("#btn");
+}
+function addItem() {
+    //Ein neues Div wird erschaffen. Die vorher eingegebenen Werte werden als <p> zu Kindern und <i> Elemente werden hinzugefügt
+    let uncheckedCircle = document.createElement("i");
+    uncheckedCircle.classList.add("fa-regular fa-circle");
+    itemContainer.appendChild(uncheckedCircle);
+    let checkedCircle = document.createElement("i");
+    checkedCircle.classList.add(".hidden");
+    itemContainer.appendChild(checkedCircle);
+    let name = document.createElement("p");
+    name.classList.add("addedItem");
+    name.innerText = selectorInput.value;
+    itemContainer.appendChild(name);
+    selectorInput.value = "";
+    let amount = document.createElement("p");
+    amount.classList.add("addedAmount");
+    amount.innerText = selectorAmount.value;
+    itemContainer.appendChild(amount);
+    selectorAmount.value = "";
+    let comment = document.createElement("p");
+    comment.classList.add("addedComment");
+    comment.innerText = selectorComment.value;
+    itemContainer.appendChild(comment);
+    selectorComment.value = "";
+    let date = document.createElement("p");
+    date.classList.add("addedDate");
+    date.innerText = selectorDate.value;
+    itemContainer.appendChild(date);
+    selectorDate.value = "";
+    let edit = document.createElement("i");
+    edit.classList.add("fa-solid fa-pen-to-square");
+    itemContainer.appendChild(edit);
+    let trash = document.createElement("i");
+    edit.classList.add("fa-solid fa-trash-can");
+    itemContainer.appendChild(trash);
+    let item = document.createElement("div");
+    item.classList.add("addedDiv");
+    item.appendChild(uncheckedCircle);
+    item.appendChild(checkedCircle);
+    item.appendChild(name);
+    item.appendChild(amount);
+    item.appendChild(comment);
+    item.appendChild(date);
+    item.appendChild(edit);
+    item.appendChild(trash);
+    itemContainer.appendChild(item);
+}
+function circleChecked() {
+    console.log("fa-regular fa-circle erhält die Klasse hidden von fa-regular fa-circle-xmark und fa-regular fa-circle-xmark ist nun sichtbar");
+    uncheckedCircle.classList.remove("fa-regular", "fa-circle");
+    uncheckedCircle.classList.add("fa-regular", "fa-circle-check-xmark");
+    if (uncheckedCircle.getAttribute("fa-circle-check")) {
         uncheckedCircle.classList.remove("fa-regular", "fa-circle");
-        uncheckedCircle.classList.add("fa-regular", "fa-circle-check-xmark");
-        if (uncheckedCircle.getAttribute("fa-circle-check")) {
-            uncheckedCircle.classList.remove("fa-regular", "fa-circle");
-            checkedCircle.classList.remove("hidden");
-            uncheckedcircle.classList.add("hidden");
-            checkedCircle.classList.add("fa-regular", "fa-circle");
-        }
-    });
-    let selectorCheckedCircle = document.querySelector(".fa-regular fa-circle-xmark");
-    selectorCheckedCircle.addEventListener("click", function () {
-        console.log("fa-regular fa-circle-xmark erhält die Klasse hidden von fa-regular fa-circle und fa-regular fa-circle ist nun sichtbar");
-    });
-    //let selectorEdit: HTMLElement = document.getElementById(".fa-solid fa-pen-to-square")as unknown as HTMLElement;
-    edit.addEventListener("click", function () {
-        console.log("Die Paragraphen werden als Kinder entfernt und durch die obrigen, passenden Text/- und Inputfelder ersetzt. Die davorigen Eingaben bleiben erhalten; Die Felder kann man wieder bearbeiten und durch einen neuen Button eingeben");
-    });
-    //let selectorTrash: HTMLElement = document.querySelector("fa-solid fa-trash-can")as unknown as HTMLElement;
-    trash.addEventListener("click", function () {
-        console.log("Das Div, dem der Trash hinzugehört wird als Kind vom itemContainer entfernt entfernt");
-        itemContainer.removeChild(item);
-    });
-});
+        checkedCircle.classList.remove("hidden");
+        uncheckedcircle.classList.add("hidden");
+        checkedCircle.classList.add("fa-regular", "fa-circle");
+    }
+}
+function deleteItem() {
+    itemContainer.removeChild(item);
+}
+function editItem() {
+}
+itemContainer.addEventListener("click", editItem);
+trash.addEventListener("click", deleteItem);
+selectorButton.addEventListener("click", addItem);
+uncheckedCircle.addEventListener("click", circleChecked);
+selectorButton.addEventListener("click", addItem);
+//Hier kann ein Item hinzugefügt werden
+//Hier kann eine Zahlenmenge des Item hinzugefügt werden
+//Hier kann ein Kommentar zum Item erstellt werden
+//Hier kann ein Datum zum Item hinzugefügt werden
+//Ein neues Div wird erschaffen. Die vorher eingegebenen Werte werden als <p> zu Kindern und <i> Elemente werden hinzugefügt
+//fa-regular fa-circle erhält die Klasse hidden von fa-regular fa-circle-xmark und fa-regular fa-circle-xmark ist nun sichtbar
+//Die Paragraphen werden als Kinder entfernt und durch die obrigen, passenden Text/- und Inputfelder ersetzt. Die davorigen Eingaben bleiben erhalten; Die Felder kann man wieder bearbeiten und durch einen neuen Button eingeben
 //# sourceMappingURL=script.js.map
--- Steckbrief/L04_Einkaufsliste_Datenstruktur/script.ts
+++ Steckbrief/L04_Einkaufsliste_Datenstruktur/script.ts
@@ -5,38 +5,19 @@ Matrikel: <271526>
 Datum: <.10.2022>
 */
 
-window.addEventListener("load", function (): void {
+window.addEventListener("load", handleLoad);
 
-    let itemContainer: HTMLDivElement = document.getElementById("itemContainer") as HTMLDivElement;
-    let selectorButton: HTMLButtonElement = document.getElementById("#btn") as HTMLButtonElement;
-    let selectorInput: HTMLElement = document.getElementById("#inputID")as HTMLElement;
-    
-    selectorInput.addEventListener("click", function(): void {
-    
-        console.log("Hier kann ein Item hinzugefügt werden.");
-    });
-    
-    let selectorAmount: HTMLElement = document.getElementById("#amountID")as unknown as HTMLElement;
-    selectorAmount.addEventListener("click", function(): void {
-    
-        console.log("Hier kann eine Zahlenmenge des Item hinzugefügt werden");
-    });
-    
-    let selectorComment: HTMLTextAreaElement = document.getElementById("#textareaID")as unknown as HTMLTextAreaElement;
-    selectorComment.addEventListener("click", function(): void {
-    
-        console.log("Hier kann ein Kommentar zum Item erstellt werden");
-    });
-    
-    let selectorDate: HTMLTextAreaElement = document.getElementById("#dateID")as unknown as HTMLTextAreaElement;
-    selectorDate.addEventListener("click", function(): void {
-    
-        console.log("Hier kann ein Datum zum Item hinzugefügt werden");
-    });
+function handleLoad(_event: Event): void {
 
-    selectorButton.addEventListener("click", function(): void {
-    
-        console.log("Ein neues Div wird erschaffen. Die vorher eingegebenen Werte werden als <p> zu Kindern und <i> Elemente werden hinzugefügt");
+let itemContainer: HTMLDivElement = document.getElementById("itemContainer") as HTMLDivElement;
+let selectorInput: HTMLElement = document.getElementById("#inputID")as HTMLElement;
+let selectorButton: HTMLButtonElement = document.getElementById("#btn") as HTMLButtonElement;
+
+}
+
+function addItem(): void {
+
+        //Ein neues Div wird erschaffen. Die vorher eingegebenen Werte werden als <p> zu Kindern und <i> Elemente werden hinzugefügt
 
         let uncheckedCircle: HTMLElement = document.createElement("i");
         uncheckedCircle.classList.add("fa-regular fa-circle");
@@ -89,41 +70,51 @@ window.addEventListener("load", function (): void {
         item.appendChild(edit);
         item.appendChild(trash);
         itemContainer.appendChild(item);
-    });
+    }
 
-    //let selectorRegularCircle: HTMLElement = document.querySelector(".fa-regular fa-circle")as unknown as HTMLElement;
-    uncheckedCircle.addEventListener("click", function(): void {
-    
+function circleChecked(): void {
         console.log("fa-regular fa-circle erhält die Klasse hidden von fa-regular fa-circle-xmark und fa-regular fa-circle-xmark ist nun sichtbar");
         uncheckedCircle.classList.remove("fa-regular", "fa-circle");
         uncheckedCircle.classList.add("fa-regular", "fa-circle-check-xmark");
-
+    
         if (uncheckedCircle.getAttribute("fa-circle-check")) {
             uncheckedCircle.classList.remove("fa-regular", "fa-circle");
             checkedCircle.classList.remove("hidden");
             uncheckedcircle.classList.add("hidden");
             checkedCircle.classList.add("fa-regular", "fa-circle");
-    
-    }});
-    
-    let selectorCheckedCircle: HTMLElement = document.querySelector(".fa-regular fa-circle-xmark")as unknown as HTMLElement;
-    selectorCheckedCircle.addEventListener("click", function(): void {
-    
-        console.log("fa-regular fa-circle-xmark erhält die Klasse hidden von fa-regular fa-circle und fa-regular fa-circle ist nun sichtbar");
-    });
-    
-    //let selectorEdit: HTMLElement = document.getElementById(".fa-solid fa-pen-to-square")as unknown as HTMLElement;
-    edit.addEventListener("click", function(): void {
-    
-        console.log("Die Paragraphen werden als Kinder entfernt und durch die obrigen, passenden Text/- und Inputfelder ersetzt. Die davorigen Eingaben bleiben erhalten; Die Felder kann man wieder bearbeiten und durch einen neuen Button eingeben");
+    }
+ }
 
-    });
+function deleteItem(): void {
+    itemContainer.removeChild(item);
+}
+
+function editItem(): void {
+
+}
+
+itemContainer.addEventListener("click", editItem);
+
+trash.addEventListener("click", deleteItem);
+
+selectorButton.addEventListener("click", addItem); 
+
+uncheckedCircle.addEventListener("click", circleChecked);
+
+selectorButton.addEventListener("click", addItem); 
+
+
     
-    //let selectorTrash: HTMLElement = document.querySelector("fa-solid fa-trash-can")as unknown as HTMLElement;
-    trash.addEventListener("click", function(): void {
+        //Hier kann ein Item hinzugefügt werden
     
-        console.log("Das Div, dem der Trash hinzugehört wird als Kind vom itemContainer entfernt entfernt");
-        itemContainer.removeChild(item);
-    });
+        //Hier kann eine Zahlenmenge des Item hinzugefügt werden
     
-    });
+        //Hier kann ein Kommentar zum Item erstellt werden
+    
+        //Hier kann ein Datum zum Item hinzugefügt werden
+    
+        //Ein neues Div wird erschaffen. Die vorher eingegebenen Werte werden als <p> zu Kindern und <i> Elemente werden hinzugefügt
+    
+        //fa-regular fa-circle erhält die Klasse hidden von fa-regular fa-circle-xmark und fa-regular fa-circle-xmark ist nun sichtbar
+    
+        //Die Paragraphen werden als Kinder entfernt und durch die obrigen, passenden Text/- und Inputfelder ersetzt. Die davorigen Eingaben bleiben erhalten; Die Felder kann man wieder bearbeiten und durch einen neuen Button eingeben
