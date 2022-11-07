ca1f1687569b4ac0223dd075d605a24ca368afe5
EmilDieterle <dieterleemil@gmail.com>
2022-11-07 06:42:53 +0100

    L04

--- Steckbrief/L04_Einkaufsliste_Datenstruktur/Data.ts
+++ Steckbrief/L04_Einkaufsliste_Datenstruktur/Data.ts
@@ -16,5 +16,5 @@ namespace L04_Datenstruktur {
             { name:"Erdbeeren", amount:20.00, comment:"Tortenbeschmückung", date:"29.10.2022" },
             { name:"Milch", amount:4.00, comment:"Milch ist für den Kakao", date:"02.11.2022" }
         ]
-    }
+    };
 }
